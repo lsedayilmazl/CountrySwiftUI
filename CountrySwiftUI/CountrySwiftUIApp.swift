d43917a88910541c3787012f050a6fc271aa6bd8
lsedayilmazl <sedayilmaz186@hotmail.com>
2024-08-13 10:09:30 +0300

    Initial Commit

---
+++ CountrySwiftUI/CountrySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountrySwiftUIApp.swift
+//  CountrySwiftUI
+//
+//  Created by Finartz on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct CountrySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
